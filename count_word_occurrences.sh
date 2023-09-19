754b2585dd0588f2cae389827640b9f85b7f29c8
Bon-1900 <bon_1900@yahoo.com>
2023-09-19 14:27:56 +0100

    FEAT: add doc and enable flags in shell script

--- count_word_occurrences.sh
+++ count_word_occurrences.sh
@@ -1,13 +1,47 @@
 #!/bin/sh
 
+input_path=""
+
+# Function to display usage information
+show_help() {
+    echo "Usage: [OPTIONS]"
+    echo "Options:"
+    echo "  -h, --help       : See documentation."
+    echo "  -p, --path PATH  : Specify a path."
+    return 1    
+}
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -h|--help)
+            show_help
+            return 0
+            ;;
+        -p|--path)
+            if [[ -n "$2" ]]; then
+                input_path="$2"
+                shift
+            else
+                echo "Error: -p or --path flag requires a path argument."
+                return 1
+            fi
+            ;;
+        *)
+            echo "Error: Unknown option '$1'"
+            show_help
+            return 1
+            ;;
+    esac
+    shift
+done
+
 # Check if an input argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <input>"
-  exit 1
+if [ -z "$input_path" ]; then
+  echo "Enter the absolute path of the word document:"
+  read userInput
+  input_path=$userInput
 fi
 
-# Store the input in a variable
-input_data=$1
-
 # Run the Python script with the provided input
-python count_word_occurrences.py "$input_data"
+python count_word_occurrences.py "$input_path"
